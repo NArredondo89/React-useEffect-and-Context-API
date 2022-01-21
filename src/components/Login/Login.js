115f2b8302e71ddba96a19aabaff047820fcb16c
Nick <86270545+NArredondo89@users.noreply.github.com>
2022-01-21 14:26:58 -0800

    use reducers with login and error handling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,7 +5,6 @@ import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
 ///the inputs in action.type can be the same between emailReducer and passwordReducer
-
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
     return { value: action.val, isValid: action.val.includes('@') };
