0303b21f46aeab9077e3a9c2ccc6fa189d6d9968
Nick <86270545+NArredondo89@users.noreply.github.com>
2022-01-21 15:08:35 -0800

    useContext Hook

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import AuthContext from '../store/auth-context';
+import React, { useContext } from 'react';
+/// import use Context
+
+import AuthContext from '../../components/store/auth-context';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+  // Create a variable to use the useContext hook
+  // Add the Variable that you are refering to, esure that you are importing it above
+  const ctx = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
